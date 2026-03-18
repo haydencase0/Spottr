c6f2138a074ca36b0e672b78381fe2d0396948cb
Jacob-Derington <der20003@byui.edu>
2026-03-18 11:15:29 -0600

    trying to make the messages more identifiable.

--- message_board/message_board.js
+++ message_board/message_board.js
@@ -1,20 +1,32 @@
 const form = document.getElementById("message-form");
+const nameInput = document.getElementById("name-input");
 const input = document.getElementById("message-input");
 const messages = document.getElementById("messages");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // stop page refresh
 
+    const name = nameInput.value.trim();
     const text = input.value.trim();
-    if (text === "") return;
+    if (name === "" || text === "") return;
 
     const message = document.createElement("div");
     message.classList.add("message");
-    message.textContent = text;
+
+    const messageText = document.createElement("div");
+    messageText.textContent = text;
+
+    const messageName = document.createElement("div");
+    messageName.classList.add("message-name");
+    messageName.textContent `- ${name}`;
+
+    message.appendChild(messageText);
+    message.appendChild(messageName);
 
     messages.appendChild(message);
 
     messages.scrollTop = messages.scrollHeight;
 
-    input.value = "";
+    nameInput.value = "";
+    messageIn
 });
